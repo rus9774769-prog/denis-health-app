b72b2966bd96cf8bbf22a7327304eb5b28bd8101
rus9774769-prog <rus9774769@gmail.com>
2026-05-08 18:57:15 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'denis-health-mobile-v3';
+const CACHE_NAME = 'denis-health-mobile-v6';
 const FILES = ['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
 self.addEventListener('install', (event) => {
   event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES)));
